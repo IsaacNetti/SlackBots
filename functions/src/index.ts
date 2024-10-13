abc524efbc6ef8a32e0395203d5454c5b5cdda19
IsaacNetti <isaacnetti@gmail.com>
2024-10-13 18:06:31 -0400

    Working Hello world

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,23 @@
 import * as functions from "firebase-functions";
-// import {WebClient} from "@slack/web-api";
+import {WebClient} from "@slack/web-api";
 
-// const bot = new WebClient(functions.config().slack.token);
+const bot = new WebClient(functions.config().slack.token);
 // const{PubSub} = require("@google-cloud/pubsub");
 // const pubsubClient = new PubSub();
 
-export const Gertie = functions.https.onRequest(async (req, res) =>{
+export const Gertie = functions.https.onRequest(async (req, res) => {
   // Obtain event and make sure it is the proper event, then respond
-  const event = req.body.event;
-  if (event && event.type === "app_mention") {
-    const response ="Me and my 482 cats say hello";
-    const slackMessage = {
-      text: response,
-      channel: event.channel,
-    };
-
-    res.status(200).send(slackMessage);
-  } else {
-    res.status(200).send({message: "event received but not processed"});
+  // const message = req.body;
+  // console.log(message);
+  try {
+    const response = await bot.chat.postMessage({
+      channel: "#general",
+      text: "Hello world",
+    });
+    console.log("Message sent", response.ts);
+  } catch (error) {
+    console.error("error sending message", error);
   }
+  res.sendStatus(200);
 });
 
