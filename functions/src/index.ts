2c2164df724389abe7f396e839f0a2bff34a2a09
Isaac Netti <isaacnetti@gmail.com>
2024-10-14 12:51:11 -0400

    Gertie now responds in the channel she was called in

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,11 @@ const bot = new WebClient(functions.config().slack.token);
 // const pubsubClient = new PubSub();
 
 export const Gertie = functions.https.onRequest(async (req, res) => {
-  // Obtain event and make sure it is the proper event, then respond
-  // const message = req.body;
-  // console.log(message);
+  // Obtain chaneel from request, then respond in that channel
+  const {event} = req.body;
   try {
     const response = await bot.chat.postMessage({
-      channel: "#general",
+      channel: event.channel,
       text: "Hello world",
     });
     console.log("Message sent", response.ts);
@@ -20,4 +19,3 @@ export const Gertie = functions.https.onRequest(async (req, res) => {
   }
   res.sendStatus(200);
 });
-
