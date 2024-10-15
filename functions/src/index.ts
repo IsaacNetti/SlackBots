7d4a50f359ee8c246ad52fee96ec44adf36d1ee5
IsaacNetti <isaacnetti@gmail.com>
2024-10-15 19:33:00 -0400

    Gertie and Jitter functional random responses

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,13 +3,14 @@ import {WebClient} from "@slack/web-api";
 import {readFileSync} from "fs";
 import {join} from "path";
 
-const bot = new WebClient(functions.config().slack.token);
+const gertie = new WebClient(functions.config().slack.token);
+const jitter = new WebClient(functions.config().slack.token1);
 // const{PubSub} = require("@google-cloud/pubsub");
 // const pubsubClient = new PubSub();
 
 // Read greetings from a text file
-const getRandomGreeting = () => {
-  const greetingsPath = join(__dirname, "textbanks/gertieRandom.txt");
+const getRandomGreeting = (botName: string) => {
+  const greetingsPath = join(__dirname, "textbanks/"+botName+"Random.txt");
   // Filter out empty lines
   const greetings = readFileSync(greetingsPath,
     "utf-8").split("\n").filter(Boolean);
@@ -20,10 +21,30 @@ const getRandomGreeting = () => {
 export const Gertie = functions.https.onRequest(async (req, res) => {
   // Obtain chaneel from request, then respond in that channel
   const {event} = req.body;
+  const botName = "gertie";
   try {
     // Pick a random greeting
-    const randomGreeting = getRandomGreeting();
-    const response = await bot.chat.postMessage({
+    const randomGreeting = getRandomGreeting(botName);
+    const response = await gertie.chat.postMessage({
+      channel: event.channel,
+      text: randomGreeting,
+    });
+    console.log("Message sent", response.ts);
+  } catch (error) {
+    console.log("Current directory:", __dirname);
+    console.error("error sending message", error);
+  }
+  res.sendStatus(200);
+});
+
+export const Jitter = functions.https.onRequest(async (req, res) => {
+  // Obtain chaneel from request, then respond in that channel
+  const {event} = req.body;
+  const botName = "jitter";
+  try {
+  //   // Pick a random greeting
+    const randomGreeting = getRandomGreeting(botName);
+    const response = await jitter.chat.postMessage({
       channel: event.channel,
       text: randomGreeting,
     });
